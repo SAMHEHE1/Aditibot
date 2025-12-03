2ed6eba684d4327402a0f7b56c7939c212ebe21f
SAMHEHE1 <sampawaryt@gmail.com>
2025-12-03 10:28:20 +0530

    Update start.py

--- PURVIMUSIC/plugins/bot/start.py
+++ PURVIMUSIC/plugins/bot/start.py
@@ -8,12 +8,12 @@ from youtubesearchpython.__future__ import VideosSearch
 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputMediaPhoto
 import config
-from PURVIMUSIC import app
-from PURVIMUSIC.misc import _boot_
-from PURVIMUSIC.plugins.sudo.sudoers import sudoers_list
-from PURVIMUSIC.utils.database import get_served_chats, get_served_users, get_sudoers
-from PURVIMUSIC.utils import bot_sys_stats
-from PURVIMUSIC.utils.database import (
+from KAISENMUSIC import app
+from KAISENMUSIC.misc import _boot_
+from KAISENMUSIC.plugins.sudo.sudoers import sudoers_list
+from KAISENMUSIC.utils.database import get_served_chats, get_served_users, get_sudoers
+from KAISENMUSIC.utils import bot_sys_stats
+from KAISENMUSIC.utils.database import (
     add_served_chat,
     add_served_user,
     blacklisted_chats,
@@ -21,9 +21,9 @@ from PURVIMUSIC.utils.database import (
     is_banned_user,
     is_on_off,
 )
-from PURVIMUSIC.utils.decorators.language import LanguageStart
-from PURVIMUSIC.utils.formatters import get_readable_time
-from PURVIMUSIC.utils.inline import help_pannel, private_panel, start_panel
+from KAISENMUSIC.utils.decorators.language import LanguageStart
+from KAISENMUSIC.utils.formatters import get_readable_time
+from KAISENMUSIC.utils.inline import help_pannel, private_panel, start_panel
 from config import BANNED_USERS
 from strings import get_string
 
