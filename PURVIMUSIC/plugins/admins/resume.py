29d9a56616400d616a1cdf847bc1552ade53dda3
SAMHEHE1 <sampawaryt@gmail.com>
2025-12-03 10:16:42 +0530

    Update resume.py

--- PURVIMUSIC/plugins/admins/resume.py
+++ PURVIMUSIC/plugins/admins/resume.py
@@ -1,11 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from PURVIMUSIC import app
-from PURVIMUSIC.core.call import PURVI
-from PURVIMUSIC.utils.database import is_music_playing, music_on
-from PURVIMUSIC.utils.decorators import AdminRightsCheck
-from PURVIMUSIC.utils.inline import close_markup
+from KAISENMUSIC import app
+from KAISENMUSIC.core.call import PURVI
+from KAISENMUSIC.utils.database import is_music_playing, music_on
+from KAISENMUSIC.utils.decorators import AdminRightsCheck
+from KAISENMUSIC.utils.inline import close_markup
 from config import BANNED_USERS
 
 
@@ -15,7 +15,7 @@ async def resume_com(cli, message: Message, _, chat_id):
     if await is_music_playing(chat_id):
         return await message.reply_text(_["admin_3"])
     await music_on(chat_id)
-    await PURVI.resume_stream(chat_id)
+    await KAISEN.resume_stream(chat_id)
     await message.reply_text(
         _["admin_4"].format(message.from_user.mention), reply_markup=close_markup(_)
     )
